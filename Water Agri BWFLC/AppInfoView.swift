424b696f2e7c2eec78603894aaba23e0aa79df1c
Aaron Mak <makaaronts@gmail.com>
2023-05-24 13:08:49 +0800

    App Update: 3.0.6

--- Water Agri BWFLC/AppInfoView.swift
+++ Water Agri BWFLC/AppInfoView.swift
@@ -12,7 +12,7 @@ struct AppInfoView: View {
         NavigationStack {
             List {
                 Section(header: Text("App Version & Description")) {
-                    Text("3.0.5 - Local App Notifications Updates & App Updates")
+                    Text("3.0.6 - App Updates")
                 }
                 Section(header: Text("Minimum System Requirements")) {
                     Text("iOS 16.0")
@@ -20,6 +20,7 @@ struct AppInfoView: View {
                 Section(header: Text("Suitable Platforms")) {
                     HStack {
                         Image(systemName: "iphone.gen2")
+                            .foregroundColor(.blue)
                         Text("iPhone")
                     }
                 }
